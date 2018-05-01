7121c32faeaad362596ecfc856fabf4446606a62
Holland Schutte <hgschutte1@gmail.com>
2018-05-01 00:47:23 -0700

    mining thread additions for dubbed lyra2zz algorithm: these parameters need to be set to ensure that work.data receives a direct copy from g_work based on the condition evaluated at line 2073

--- ccminer.cpp
+++ ccminer.cpp
@@ -54,6 +54,7 @@
 #include "sia/sia-rpc.h"
 #include "crypto/xmr-rpc.h"
 #include "equi/equihash.h"
+#include "lyra2/Lyra2Z.h"
 
 #include <cuda_runtime.h>
 
@@ -1964,6 +1965,10 @@ static void *miner_thread(void *userdata)
 		} else if (opt_algo == ALGO_EQUIHASH) {
 			nonceptr = &work.data[EQNONCE_OFFSET]; // 27 is pool extranonce (256bits nonce space)
 			wcmplen = 4+32+32;
+		} else if (opt_algo == ALGO_LYRA2ZZ) {
+			// note that nonce lies right before accumulator checkpoint
+			nonceptr = work.data + LYRA2ZZ_BLOCK_HEADER_NONCE_OFFSET;
+			wcmplen = LYRA2ZZ_BLOCK_HEADER_LEN_BYTES;
 		}
 
 		if (have_stratum) {
