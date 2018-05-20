f4ade917ef7ba3024484966e5a4048cdd2ce7863
Holland Schutte <hgschutte1@gmail.com>
2018-05-20 00:50:54 -0700

    These do appear to be the correct modifications for blake hashes. Will want to test more though.

--- Algo256/cuda_blake256.cu
+++ Algo256/cuda_blake256.cu
@@ -33,7 +33,7 @@ static __device__ uint64_t cuda_swab32ll(uint64_t x) {
 
 __constant__ static uint32_t c_data[3 + 1];
 
-__constant__ static uint32_t c_data_112[7 + 1];
+__constant__ static uint32_t c_data_112[11 + 1];
 
 __constant__ static uint32_t sigma[16][16];
 static uint32_t  c_sigma[16][16] = {
@@ -141,11 +141,20 @@ __device__ __constant__ static const uint32_t  c_Padding[16] = {
 	0, 1, 0, 640,
 };
 
+/*
 __device__ __constant__ static const uint32_t  c_Padding_112[16] = {
 	0, 0, 0, 0,
 	0x80000000, 0, 0, 0,
 	0, 0, 0, 0,
 	0, 1, 0, 640,
+};
+*/
+
+__device__ __constant__ static const uint32_t c_Padding_112[16] = {
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0x80000000, 1, 0, 0x380 
 };
 
 __host__ __forceinline__
@@ -251,9 +260,17 @@ static void blake256_compress2nd_112(uint32_t *h, const uint32_t *block, const u
 	m[1] = block[1];
 	m[2] = block[2];
 	m[3] = block[3];
+	m[4] = block[4];
+	m[5] = block[5];
+	m[6] = block[6];
+	m[7] = block[7];
+	m[8] = block[8];
+	m[9] = block[9];
+	m[10] = block[10];
+	m[11] = block[11];
 
 	#pragma unroll
-	for (int i = 4; i < 16; i++) {
+	for (int i = 12; i < 16; i++) {
 		m[i] = c_Padding_112[i];
 	}
 
@@ -591,8 +608,9 @@ void blake256_gpu_hash_112(const uint32_t threads, const uint32_t startNonce, ui
 	uint32_t thread = (blockDim.x * blockIdx.x + threadIdx.x);
 	if (thread < threads)
 	{
+		const uint32_t sz = sizeof(c_data_112) / sizeof(c_data_112[0]);
 		uint32_t h[8];
-		uint32_t input[8];
+		uint32_t input[sz];
 
 		#pragma unroll
 		for (int i = 0; i < 8; i++) h[i] = cpu_h[i];
@@ -602,7 +620,9 @@ void blake256_gpu_hash_112(const uint32_t threads, const uint32_t startNonce, ui
 
 		input[3] = startNonce + thread;
 
-		blake256_compress2nd_112(h, input, 640);
+		for (int i = 4; i < 12; ++i) input[i] = c_data_112[i];
+
+		blake256_compress2nd_112(h, input, 0x380);
 
 		#pragma unroll
 		for (int i = 0; i<4; i++) {
